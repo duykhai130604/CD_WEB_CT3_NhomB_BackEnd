b2ea307b7b6a3387bb0730db98e0571175d3a184
duykhai130604 <doduykhai1306.azure@gmail.com>
2024-10-19 00:00:03 +0700

    update

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+    public function getAllProducts(Request $request)
+    {
+        $keyword = $request->input('keyword');
+        $query = Product::query();
+        if ($keyword) {
+            $query->where('name', 'like', "%$keyword%");
+        }
+        $products = $query->orderBy('updated_at', 'desc')->paginate(2);
+        return response()->json($products);
+    }
+    
+}
