02042965d1d8eec838e28b83ed2daba75123b118
duykhai130604 <doduykhai1306.azure@gmail.com>
2024-10-22 12:22:51 +0700

    Update delete ptoduct

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,21 +9,26 @@ class ProductController extends Controller
 {
     public function getAllProducts(Request $request)
     {
-        $keyword = $request->input('keyword');
-        return Product::getAllProducts($keyword);
+        
+        return Product::getAllProducts($request);
+    }
+
+    public function getProductDetails(Request $request)
+    {
+        return Product::getProductDetails($request);
     }
 
     public function addProduct(Request $request)
     {
         return Product::addProduct($request);
     }
+
     public function editProduct(Request $request)
     {
         return Product::editProduct($request);
     }
-    public function getProductDetails(Request $request)
-    {
-        $encodedId = $request->input('encodedId');
-        return Product::getProductDetails($encodedId);
+    
+    public function deleteProduct(Request $request) {
+        return Product::destroy($request);
     }
 }
